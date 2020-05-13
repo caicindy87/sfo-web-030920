8d1ccdd3e30cd52659b835bf53fda8842026be2e
Amelie Oller <amelieoller@gmail.com>
2020-05-13 14:47:59 -0700

    Finish pets review

--- 33-react-props-and-state-pets-review/src/components/App.js
+++ 33-react-props-and-state-pets-review/src/components/App.js
@@ -1,19 +1,80 @@
-import React from 'react'
+import React from 'react';
 
-import Filters from './Filters'
-import PetBrowser from './PetBrowser'
+import Filters from './Filters';
+import PetBrowser from './PetBrowser';
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       pets: [],
       filters: {
-        type: 'all'
-      }
-    }
+        type: 'all',
+      },
+    };
+
+    // this.state = {
+    //   pets: [],
+    //   filters: {
+    //     oneMoreLevel: {
+    //       type: 'all',
+    //       otherAttribute: 'hello',
+    //     },
+    //     anotherAttr: 'test',
+    //   },
+    // };
   }
+
+  onChangeType = (newType) => {
+    this.setState({
+      filters: {
+        type: newType,
+      },
+    });
+
+    // this.setState({
+    //   filters: {
+    //     ...this.state.filters,
+    //     oneMoreLevel: {
+    //       ...this.state.filters.oneMoreLevel,
+    //       type: newType,
+    //     },
+    //   },
+    // });
+  };
+
+  fetchPets = () => {
+    const { type } = this.state.filters;
+    let endpoint = `/api/pets`;
+    // let endpoint = `/api/pets${type !== 'all' ? `?type=${type}` : ''}`;
+
+    if (type !== 'all') {
+      endpoint += `?type=${type}`;
+    }
+
+    fetch(endpoint)
+      .then((resp) => resp.json())
+      .then((pets) =>
+        this.setState({
+          pets: pets,
+        })
+      );
+  };
+
+  onFindPetsClick = () => {
+    this.fetchPets();
+  };
+
+  onAdoptPet = (petId) => {
+    this.setState((prevState) => {
+      return {
+        pets: prevState.pets.map((pet) =>
+          pet.id === petId ? { ...pet, isAdopted: true } : pet
+        ),
+      };
+    });
+  };
 
   render() {
     return (
@@ -24,16 +85,19 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                onChangeType={this.onChangeType}
+                onFindPetsClick={this.onFindPetsClick}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
