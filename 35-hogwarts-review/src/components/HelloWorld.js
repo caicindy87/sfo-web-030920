5cedc7159ed80bec3393d49c100e13ee74f1670e
Amelie Oller <amelieoller@gmail.com>
2020-05-15 09:55:24 -0700

    Create hog lecture

---
+++ 35-hogwarts-review/src/components/HelloWorld.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello World</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default HelloWorld;
