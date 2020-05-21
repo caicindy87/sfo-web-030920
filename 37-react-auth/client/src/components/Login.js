6e61d3d09202b759bff9eb7ea4800f418ef42932
Amelie Oller <amelieoller@gmail.com>
2020-05-21 10:12:07 -0700

    Add bugs to auth lecture

--- 37-react-auth/client/src/components/Login.js
+++ 37-react-auth/client/src/components/Login.js
@@ -1,5 +1,5 @@
-import React from "react";
-import api from "../services/api";
+import React from 'react';
+import api from '../services/api';
 
 class Login extends React.Component {
   constructor() {
@@ -7,30 +7,26 @@ class Login extends React.Component {
     this.state = {
       error: false,
       fields: {
-        username: "",
-        password: ""
-      }
+        username: '',
+        password: '',
+      },
     };
   }
 
-  handleChange = e => {
+  handleChange = (e) => {
     const newFields = { ...this.state.fields, [e.target.name]: e.target.value };
     this.setState({ fields: newFields });
   };
 
-  handleSubmit = e => {
-    e.preventDefault()
-    
-    api.auth
-      .login(this.state.fields.username, this.state.fields.password)
-      .then(res => {
-        if (res.error) {
-          this.setState({ error: true });
-        } else {
-          this.props.handleLogin(res);
-          this.props.history.push("/");
-        }
-      });
+  handleSubmit = (e) => {
+    api.auth.login(this.state.fields.username, this.state.fields.password).then((res) => {
+      if (res.error) {
+        this.setState({ error: true });
+      } else {
+        this.props.handleLogin(res);
+        this.props.history.push('/');
+      }
+    });
   };
 
   render() {
@@ -54,7 +50,7 @@ class Login extends React.Component {
               <label>Password</label>
               <input
                 name="password"
-                type="password"
+                type="text"
                 placeholder="password"
                 value={fields.password}
                 onChange={this.handleChange}
